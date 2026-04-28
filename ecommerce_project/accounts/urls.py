7c142e0f7316d1583d293ad6d2eeeddb2aa5b581
Mani <m45589207@gmail.com>
2026-04-28 11:21:30 +0530

    urlpattern updated

--- ecommerce_project/accounts/urls.py
+++ ecommerce_project/accounts/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('signup/', signup_api, name='signup'),
     path('home/<int:id>/', home_api, name='home'),
     path('signin/', signin_api, name='signin'),
-    # path('csrf/', get_csrf),
 ]
